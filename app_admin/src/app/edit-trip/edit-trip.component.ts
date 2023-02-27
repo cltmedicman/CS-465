92a6ff487bfeedf60d757d8a0249c9a657a496ef
chriscoding1 <cltmedicman@gmail.com>
2023-02-27 00:16:40 -0500

    New commit

---
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-trip',
+  templateUrl: './edit-trip.component.html',
+  styleUrls: ['./edit-trip.component.css']
+})
+export class EditTripComponent implements OnInit {
+
+  editForm: FormGroup;
+  tripService: any;
+  submitted: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    let tripCode = localStorage.getItem("tripCode");
+    if (!tripCode) {
+      alert("Something wrong, couldn't find where I stashed tripCode!");
+      this.router.navigate(['']);
+      return;
+    }
+
+    this.editForm = this.formBuilder.group({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required],
+    })
+
+    this.tripService.getTrip(tripCode)
+      .then(data => {
+        console.log(data);
+        this.editForm.patchValue(data);
+      })
+  }
+  
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.editForm.valid) {
+      this.tripService.updateTrip(this.editForm.value)
+        .then(data => {
+          console.log(data);
+          this.router.navigate(['']);
+        });
+    }
+  }
+
+}
---
+++ app_admin/src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../models/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public fromError: string = '';
+
+  public credentials = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  public onLoginSubmit(): void {
+    this.fromError = '';
+    if(!this.credentials.email || !this.credentials.password) {
+      this.fromError = 'All fields are required, please try again';
+    } else {
+      this.doLogin();
+    }
+  }
+
+  private doLogin(): void {
+    this.authenticationService.login(this.credentials)
+      .then(() => this.router.navigateByUrl('#'))
+      .catch((message) => this.fromError = message);
+  }
+
+}
